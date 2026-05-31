59e02f5a7d52ba42abc37a698d0ac9631ac08581
primeboot699-afk <primeboot699@gmail.com>
2026-05-31 12:31:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+document.addEventListener("mousemove", (e) => {
+  const x = (window.innerWidth / 2 - e.pageX) / 50;
+  const y = (window.innerHeight / 2 - e.pageY) / 50;
+
+  document.body.style.backgroundPosition = `${50 - x}% ${50 - y}%`;
+});
+
 console.log("Prime X Gaming Loaded");
